969bad92e67e3a70f73bd9402a1756b98d290170
Thyon3 <asnakemengesha80@gmail.com>
2024-03-11 13:30:00 -0400

    feat: add app configuration class with default settings

---
+++ lib/app_config.dart
@@ -0,0 +1,11 @@
+class AppConfig {
+  static const String appName = 'Flutter Recipe App';
+  static const String appVersion = '1.0.0';
+  static const bool debugMode = true;
+  
+  static const Map<String, dynamic> defaultSettings = {
+    'theme': 'light',
+    'fontSize': 14.0,
+    'enableNotifications': true,
+  };
+}
