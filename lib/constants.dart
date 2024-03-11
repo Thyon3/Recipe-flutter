3d06408facb5c2842207cbb5b0c9206379f1d3b5
Thyon3 <asnakemengesha80@gmail.com>
2024-03-11 16:30:00 -0400

    feat: expand app constants with UI and performance settings

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,4 +6,11 @@ class AppConstants {
   static const String fontFamily = 'Roboto';
   static const Color primaryColor = Color(0xFF6200EE);
   static const Color secondaryColor = Color(0xFF03DAC6);
+  
+  static const double cardElevation = 4.0;
+  static const double buttonHeight = 48.0;
+  static const double iconSize = 24.0;
+  
+  static const int maxRetries = 3;
+  static const Duration debounceDelay = Duration(milliseconds: 500);
 }
