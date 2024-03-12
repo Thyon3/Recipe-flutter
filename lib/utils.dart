d4c69608842ed33e67f3d44f9dce97478b4b12d7
Thyon3 <asnakemengesha80@gmail.com>
2024-03-12 10:45:00 -0400

    feat: add utility functions for time formatting and validation

--- lib/utils.dart
+++ lib/utils.dart
@@ -17,4 +17,20 @@ class AppUtils {
   static bool isEven(int number) {
     return number % 2 == 0;
   }
+  
+  static String formatTime(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return "${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds";
+  }
+  
+  static String capitalize(String text) {
+    if (text.isEmpty) return text;
+    return text[0].toUpperCase() + text.substring(1).toLowerCase();
+  }
+  
+  static bool isValidEmail(String email) {
+    return RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(email);
+  }
 }
