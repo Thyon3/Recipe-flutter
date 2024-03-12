f6ca5ae505f7bb2e5bf7f5253485b1cb89a47e2f
Thyon3 <asnakemengesha80@gmail.com>
2024-03-12 09:30:00 -0400

    feat: add app constants for design system

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+class AppConstants {
+  static const double defaultPadding = 16.0;
+  static const double borderRadius = 8.0;
+  static const Duration animationDuration = Duration(milliseconds: 300);
+  
+  static const String fontFamily = 'Roboto';
+  static const Color primaryColor = Color(0xFF6200EE);
+  static const Color secondaryColor = Color(0xFF03DAC6);
+}
