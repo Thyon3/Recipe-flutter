2fd613a0bf3acd9658307b8c8102505da9748c08
Thyon3 <asnakemengesha80@gmail.com>
2024-03-18 11:00:00 -0400

    feat: add action stream and setValue method to counter service

--- lib/services/counter_service.dart
+++ lib/services/counter_service.dart
@@ -4,14 +4,17 @@ import 'package:flutter/foundation.dart';
 class CounterService {
   static int _currentValue = 0;
   static final StreamController<int> _counterController = StreamController<int>.broadcast();
+  static final StreamController<String> _actionController = StreamController<String>.broadcast();
   
   static Stream<int> get counterStream => _counterController.stream;
+  static Stream<String> get actionStream => _actionController.stream;
   
   static int get currentValue => _currentValue;
   
   static void increment() {
     _currentValue++;
     _counterController.add(_currentValue);
+    _actionController.add('increment');
     debugPrint('Counter incremented to: $_currentValue');
   }
   
@@ -19,6 +22,7 @@ class CounterService {
     if (_currentValue > 0) {
       _currentValue--;
       _counterController.add(_currentValue);
+      _actionController.add('decrement');
       debugPrint('Counter decremented to: $_currentValue');
     }
   }
@@ -26,10 +30,19 @@ class CounterService {
   static void reset() {
     _currentValue = 0;
     _counterController.add(_currentValue);
+    _actionController.add('reset');
     debugPrint('Counter reset to: $_currentValue');
+  }
+  
+  static void setValue(int value) {
+    _currentValue = value;
+    _counterController.add(_currentValue);
+    _actionController.add('setValue');
+    debugPrint('Counter set to: $_currentValue');
   }
   
   static void dispose() {
     _counterController.close();
+    _actionController.close();
   }
 }
