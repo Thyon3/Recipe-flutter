cf1f6a487ec9f7d73ffa6f48d6a371e58a908c01
Thyon3 <asnakemengesha80@gmail.com>
2024-03-20 10:30:00 -0400

    feat: enhance app theme with comprehensive text and icon themes

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -26,10 +26,15 @@ class AppTheme {
         ),
       ),
       cardTheme: CardTheme(
-        elevation: 2,
+        elevation: AppConstants.cardElevation,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(AppConstants.borderRadius),
         ),
+      ),
+      textTheme: _buildTextTheme(Brightness.light),
+      iconTheme: const IconThemeData(
+        color: AppConstants.primaryColor,
+        size: AppConstants.iconSize,
       ),
     );
   }
@@ -58,11 +63,71 @@ class AppTheme {
         ),
       ),
       cardTheme: CardTheme(
-        elevation: 2,
+        elevation: AppConstants.cardElevation,
         color: Colors.grey[800],
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(AppConstants.borderRadius),
         ),
+      ),
+      textTheme: _buildTextTheme(Brightness.dark),
+      iconTheme: const IconThemeData(
+        color: Colors.white,
+        size: AppConstants.iconSize,
+      ),
+    );
+  }
+  
+  static TextTheme _buildTextTheme(Brightness brightness) {
+    final Color textColor = brightness == Brightness.light ? Colors.black : Colors.white;
+    final Color secondaryTextColor = brightness == Brightness.light ? Colors.grey[600]! : Colors.grey[400]!;
+    
+    return TextTheme(
+      headlineLarge: TextStyle(
+        fontSize: 32,
+        fontWeight: FontWeight.bold,
+        color: textColor,
+      ),
+      headlineMedium: TextStyle(
+        fontSize: 28,
+        fontWeight: FontWeight.bold,
+        color: textColor,
+      ),
+      headlineSmall: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+        color: textColor,
+      ),
+      titleLarge: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w600,
+        color: textColor,
+      ),
+      titleMedium: TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.w500,
+        color: textColor,
+      ),
+      titleSmall: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+        color: textColor,
+      ),
+      bodyLarge: TextStyle(
+        fontSize: 16,
+        color: textColor,
+      ),
+      bodyMedium: TextStyle(
+        fontSize: 14,
+        color: textColor,
+      ),
+      bodySmall: TextStyle(
+        fontSize: 12,
+        color: secondaryTextColor,
+      ),
+      labelLarge: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        color: textColor,
       ),
     );
   }
