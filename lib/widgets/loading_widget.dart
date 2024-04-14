1f0b9e0fa23e5d4b086f0bd5704f70406a8b0e5f
Thyon3 <asnakemengesha80@gmail.com>
2024-04-14 09:30:00 -0400

    feat: enhance loading widget with pulsing animation and customization

--- lib/widgets/loading_widget.dart
+++ lib/widgets/loading_widget.dart
@@ -4,11 +4,17 @@ import '../constants.dart';
 class LoadingWidget extends StatelessWidget {
   final String? message;
   final double? size;
+  final Color? color;
+  final double? strokeWidth;
+  final String? loadingText;
   
   const LoadingWidget({
     super.key,
     this.message,
     this.size,
+    this.color,
+    this.strokeWidth,
+    this.loadingText,
   });
   
   @override
@@ -21,9 +27,9 @@ class LoadingWidget extends StatelessWidget {
             width: size ?? 40,
             height: size ?? 40,
             child: CircularProgressIndicator(
-              strokeWidth: 3,
+              strokeWidth: strokeWidth ?? 3,
               valueColor: AlwaysStoppedAnimation<Color>(
-                AppConstants.primaryColor,
+                color ?? AppConstants.primaryColor,
               ),
             ),
           ),
@@ -31,11 +37,21 @@ class LoadingWidget extends StatelessWidget {
             const SizedBox(height: 16),
             Text(
               message!,
-              style: const TextStyle(
+              style: TextStyle(
                 fontSize: 16,
-                color: Colors.grey,
+                color: Colors.grey[600],
               ),
               textAlign: TextAlign.center,
+            ),
+          ],
+          if (loadingText != null) ...[
+            const SizedBox(height: 8),
+            Text(
+              loadingText!,
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.grey[400],
+              ),
             ),
           ],
         ],
@@ -48,12 +64,18 @@ class LoadingOverlay extends StatelessWidget {
   final Widget child;
   final bool isLoading;
   final String? loadingMessage;
+  final double? size;
+  final Color? overlayColor;
+  final bool dismissible;
   
   const LoadingOverlay({
     super.key,
     required this.child,
     required this.isLoading,
     this.loadingMessage,
+    this.size,
+    this.overlayColor,
+    this.dismissible = false,
   });
   
   @override
@@ -62,14 +84,79 @@ class LoadingOverlay extends StatelessWidget {
       children: [
         child,
         if (isLoading)
-          Container(
-            color: Colors.black.withOpacity(0.5),
-            child: LoadingWidget(
-              message: loadingMessage ?? 'Loading...',
-              size: 50,
+          GestureDetector(
+            onTap: dismissible ? null : () {},
+            child: Container(
+              color: overlayColor ?? Colors.black.withOpacity(0.5),
+              child: LoadingWidget(
+                message: loadingMessage ?? 'Loading...',
+                size: size ?? 50,
+              ),
             ),
           ),
       ],
+    );
+  }
+}
+
+class PulsingLoadingWidget extends StatefulWidget {
+  final String? message;
+  final double? size;
+  final Color? color;
+  
+  const PulsingLoadingWidget({
+    super.key,
+    this.message,
+    this.size,
+    this.color,
+  });
+  
+  @override
+  State<PulsingLoadingWidget> createState() => _PulsingLoadingWidgetState();
+}
+
+class _PulsingLoadingWidgetState extends State<PulsingLoadingWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1500),
+      vsync: this,
+    );
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    );
+    _controller.repeat();
+  }
+  
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return Transform.scale(
+          scale: 1.0 + (_animation.value * 0.2),
+          child: Opacity(
+            opacity: 0.6 + (_animation.value * 0.4),
+            child: LoadingWidget(
+              message: widget.message,
+              size: widget.size,
+              color: widget.color,
+            ),
+          ),
+        );
+      },
     );
   }
 }
