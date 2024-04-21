44be38882c3e4be76b7163c2818a2f9348dbb5aa
Thyon3 <asnakemengesha80@gmail.com>
2024-04-21 09:30:00 -0400

    feat: enhance rating widget with alignment and padding options

--- lib/widgets/rating_widget.dart
+++ lib/widgets/rating_widget.dart
@@ -1,115 +1,197 @@
 import 'package:flutter/material.dart';
 import '../constants.dart';
 
-class RatingWidget extends StatelessWidget {
+class RatingWidget extends StatefulWidget {
   final double rating;
   final int maxRating;
+  final Function(double)? onRatingChanged;
   final double size;
   final Color? color;
-  final bool interactive;
-  final Function(double)? onRatingChanged;
+  final bool isInteractive;
+  final bool showHalfStars;
+  final bool showRatingValue;
+  final String? ratingText;
+  final EdgeInsetsGeometry? padding;
+  final MainAxisAlignment alignment;
   
   const RatingWidget({
     super.key,
     required this.rating,
     this.maxRating = 5,
-    this.size = 20,
-    this.color,
-    this.interactive = false,
     this.onRatingChanged,
+    this.size = 24.0,
+    this.color,
+    this.isInteractive = true,
+    this.showHalfStars = true,
+    this.showRatingValue = false,
+    this.ratingText,
+    this.padding,
+    this.alignment = MainAxisAlignment.center,
   });
   
   @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: List.generate(maxRating, (index) {
-        return _buildStar(index);
-      }),
-    );
-  }
-  
-  Widget _buildStar(int index) {
-    final starValue = index + 1;
-    final filled = rating >= starValue;
-    final halfFilled = rating > index && rating < starValue;
-    
-    Widget star;
-    
-    if (halfFilled) {
-      star = Icon(
-        Icons.star_half,
-        color: color ?? Colors.amber,
-        size: size,
-      );
-    } else if (filled) {
-      star = Icon(
-        Icons.star,
-        color: color ?? Colors.amber,
-        size: size,
-      );
-    } else {
-      star = Icon(
-        Icons.star_border,
-        color: color ?? Colors.amber,
-        size: size,
-      );
-    }
-    
-    if (interactive && onRatingChanged != null) {
-      return GestureDetector(
-        onTap: () => onRatingChanged!(starValue.toDouble()),
-        child: star,
-      );
-    }
-    
-    return star;
-  }
+  State<RatingWidget> createState() => _RatingWidgetState();
 }
 
-class InteractiveRatingWidget extends StatefulWidget {
-  final double rating;
-  final int maxRating;
-  final double size;
-  final Color? color;
-  final Function(double)? onRatingChanged;
-  
-  const InteractiveRatingWidget({
-    super.key,
-    required this.rating,
-    this.maxRating = 5,
-    this.size = 24,
-    this.color,
-    this.onRatingChanged,
-  });
-  
-  @override
-  State<InteractiveRatingWidget> createState() => _InteractiveRatingWidgetState();
-}
-
-class _InteractiveRatingWidgetState extends State<InteractiveRatingWidget> {
-  late double _currentRating;
+class _RatingWidgetState extends State<RatingWidget> {
+  late double _rating;
   
   @override
   void initState() {
     super.initState();
-    _currentRating = widget.rating;
+    _rating = widget.rating;
   }
   
   @override
   Widget build(BuildContext context) {
-    return RatingWidget(
-      rating: _currentRating,
-      maxRating: widget.maxRating,
-      size: widget.size,
-      color: widget.color,
-      interactive: true,
-      onRatingChanged: (newRating) {
+    return Padding(
+      padding: widget.padding ?? EdgeInsets.zero,
+      child: Row(
+        mainAxisAlignment: widget.alignment,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: List.generate(widget.maxRating, (index) {
+              return _buildStar(index);
+            }),
+          ),
+          if (widget.showRatingValue) ...[
+            const SizedBox(width: 8),
+            Text(
+              widget.ratingText ?? '${_rating.toStringAsFixed(1)}/${widget.maxRating}',
+              style: TextStyle(
+                fontSize: widget.size * 0.6,
+                color: Colors.grey[600],
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ],
+      ),
+    );
+  }
+  
+  Widget _buildStar(int index) {
+    final starValue = index + 1.0;
+    final halfStarValue = index + 0.5;
+    bool isFilled = false;
+    bool isHalfFilled = false;
+    
+    if (widget.showHalfStars) {
+      if (_rating >= starValue) {
+        isFilled = true;
+      } else if (_rating >= halfStarValue) {
+        isHalfFilled = true;
+      }
+    } else {
+      isFilled = _rating >= starValue;
+    }
+    
+    return GestureDetector(
+      onTap: widget.isInteractive ? () {
         setState(() {
-          _currentRating = newRating;
+          _rating = starValue.toDouble();
+          widget.onRatingChanged?.call(_rating);
         });
-        widget.onRatingChanged?.call(newRating);
-      },
+      } : null,
+      onTapDown: widget.isInteractive && widget.showHalfStars ? (details) {
+        final localDx = details.localPosition.dx;
+        final starWidth = widget.size;
+        final isHalf = localDx < starWidth / 2;
+        final newRating = isHalf ? halfStarValue : starValue;
+        
+        setState(() {
+          _rating = newRating;
+          widget.onRatingChanged?.call(_rating);
+        });
+      } : null,
+      child: Container(
+        width: widget.size,
+        height: widget.size,
+        child: Stack(
+          children: [
+            Icon(
+              Icons.star_border,
+              size: widget.size,
+              color: widget.color ?? Colors.grey[300],
+            ),
+            if (isFilled)
+              Icon(
+                Icons.star,
+                size: widget.size,
+                color: widget.color ?? Colors.amber,
+              ),
+            if (isHalfFilled)
+              ClipRect(
+                clipper: HalfClipper(
+                  clipWidth: widget.size / 2,
+                ),
+                child: Icon(
+                  Icons.star,
+                  size: widget.size,
+                  color: widget.color ?? Colors.amber,
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HalfClipper extends CustomClipper<Rect> {
+  final double clipWidth;
+  
+  HalfClipper({required this.clipWidth});
+  
+  @override
+  Rect getClip(Size size) {
+    return Rect.fromLTWH(0, 0, clipWidth, size.height);
+  }
+  
+  @override
+  bool shouldReclip(covariant CustomClipper<Rect> oldClipper) {
+    return oldClipper is HalfClipper && oldClipper.clipWidth != clipWidth;
+  }
+}
+
+class ReadOnlyRatingWidget extends StatelessWidget {
+  final double rating;
+  final int maxRating;
+  final double size;
+  final Color? color;
+  final bool showHalfStars;
+  final bool showRatingValue;
+  final String? ratingText;
+  final EdgeInsetsGeometry? padding;
+  final MainAxisAlignment alignment;
+  
+  const ReadOnlyRatingWidget({
+    super.key,
+    required this.rating,
+    this.maxRating = 5,
+    this.size = 24.0,
+    this.color,
+    this.showHalfStars = true,
+    this.showRatingValue = false,
+    this.ratingText,
+    this.padding,
+    this.alignment = MainAxisAlignment.center,
+  });
+  
+  @override
+  Widget build(BuildContext context) {
+    return RatingWidget(
+      rating: rating,
+      maxRating: maxRating,
+      size: size,
+      color: color,
+      isInteractive: false,
+      showHalfStars: showHalfStars,
+      showRatingValue: showRatingValue,
+      ratingText: ratingText,
+      padding: padding,
+      alignment: alignment,
     );
   }
 }
