eb348ebb336c74126bd6c58c7a68cc5e8094f4d4
Thyon3 <asnakemengesha80@gmail.com>
2024-04-24 10:00:00 -0400

    feat: enhance floating action button with speed dial improvements

--- lib/widgets/floating_action_button.dart
+++ lib/widgets/floating_action_button.dart
@@ -3,170 +3,318 @@ import '../constants.dart';
 
 class CustomFloatingActionButton extends StatelessWidget {
   final VoidCallback onPressed;
-  final IconData icon;
+  final Widget child;
   final String? tooltip;
   final Color? backgroundColor;
   final Color? foregroundColor;
+  final double? elevation;
+  final double? size;
+  final bool mini;
+  final ShapeBorder? shape;
+  final Clip clipBehavior;
+  final bool autofocus;
+  final MaterialTapTargetSize? materialTapTargetSize;
   final bool isExtended;
-  final String? label;
-  
+  final Widget? label;
+  final Widget? icon;
+  final EdgeInsetsGeometry? extendedPadding;
+  final double? extendedIconLabelSpacing;
+
   const CustomFloatingActionButton({
     super.key,
     required this.onPressed,
-    required this.icon,
+    required this.child,
     this.tooltip,
     this.backgroundColor,
     this.foregroundColor,
+    this.elevation,
+    this.size,
+    this.mini = false,
+    this.shape,
+    this.clipBehavior = Clip.none,
+    this.autofocus = false,
+    this.materialTapTargetSize,
     this.isExtended = false,
     this.label,
+    this.icon,
+    this.extendedPadding,
+    this.extendedIconLabelSpacing,
   });
-  
+
   @override
   Widget build(BuildContext context) {
-    if (isExtended && label != null) {
+    if (isExtended) {
       return FloatingActionButton.extended(
         onPressed: onPressed,
-        icon: Icon(icon),
-        label: Text(label!),
+        icon: icon ?? const Icon(Icons.add),
+        label: label ?? const Text('Add'),
         tooltip: tooltip,
         backgroundColor: backgroundColor ?? AppConstants.primaryColor,
         foregroundColor: foregroundColor ?? Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(AppConstants.borderRadius),
-        ),
+        elevation: elevation ?? 6.0,
+        shape: shape,
+        clipBehavior: clipBehavior,
+        autofocus: autofocus,
+        materialTapTargetSize: materialTapTargetSize,
+        extendedPadding: extendedPadding,
+        extendedIconLabelSpacing: extendedIconLabelSpacing,
       );
     }
-    
+
     return FloatingActionButton(
       onPressed: onPressed,
+      child: child,
       tooltip: tooltip,
       backgroundColor: backgroundColor ?? AppConstants.primaryColor,
       foregroundColor: foregroundColor ?? Colors.white,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(AppConstants.borderRadius),
-      ),
-      child: Icon(icon),
+      elevation: elevation ?? 6.0,
+      mini: mini,
+      shape: shape,
+      clipBehavior: clipBehavior,
+      autofocus: autofocus,
+      materialTapTargetSize: materialTapTargetSize,
     );
   }
 }
 
 class SpeedDialFab extends StatefulWidget {
-  final List<SpeedDialItem> items;
-  final IconData mainIcon;
-  final String? mainTooltip;
+  final List<SpeedDialChild> children;
+  final Widget? icon;
+  final Widget? activeIcon;
+  final String? tooltip;
   final Color? backgroundColor;
   final Color? foregroundColor;
-  
+  final Color? activeBackgroundColor;
+  final Color? activeForegroundColor;
+  final double elevation;
+  final double? size;
+  final bool mini;
+  final ShapeBorder? shape;
+  final Curve curve;
+  final Duration animationDuration;
+  final bool openCloseDialOnValueChange;
+  final bool renderOverlay;
+  final Color? overlayColor;
+  final double? overlayOpacity;
+  final bool visible;
+  final VoidCallback? onOpen;
+  final VoidCallback? onClose;
+  final bool useRipple;
+  final double? rippleColor;
+  final double? heroTag;
+  final bool closeManually;
+  final double? spacing;
+  final double? margin;
+  final bool useRotationAnimation;
+  final double? rotationAngle;
+  final bool enableAnimation;
+  final bool switchLabelPosition;
+
   const SpeedDialFab({
     super.key,
-    required this.items,
-    required this.mainIcon,
-    this.mainTooltip,
+    required this.children,
+    this.icon,
+    this.activeIcon,
+    this.tooltip,
     this.backgroundColor,
     this.foregroundColor,
+    this.activeBackgroundColor,
+    this.activeForegroundColor,
+    this.elevation = 6.0,
+    this.size,
+    this.mini = false,
+    this.shape,
+    this.curve = Curves.easeInOut,
+    this.animationDuration = const Duration(milliseconds: 200),
+    this.openCloseDialOnValueChange = false,
+    this.renderOverlay = true,
+    this.overlayColor,
+    this.overlayOpacity,
+    this.visible = true,
+    this.onOpen,
+    this.onClose,
+    this.useRipple = true,
+    this.rippleColor,
+    this.heroTag,
+    this.closeManually = false,
+    this.spacing,
+    this.margin,
+    this.useRotationAnimation = true,
+    this.rotationAngle = 45.0,
+    this.enableAnimation = true,
+    this.switchLabelPosition = false,
   });
-  
+
   @override
   State<SpeedDialFab> createState() => _SpeedDialFabState();
 }
 
-class _SpeedDialFabState extends State<SpeedDialFab>
-    with SingleTickerProviderStateMixin {
+class _SpeedDialFabState extends State<SpeedDialFab> with TickerProviderStateMixin {
   late AnimationController _controller;
-  late Animation<double> _animation;
+  late Animation<double> _expandAnimation;
+  late Animation<double> _rotateAnimation;
   bool _isOpen = false;
-  
+
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
-      duration: AppConstants.animationDuration,
+      duration: widget.animationDuration,
       vsync: this,
     );
-    _animation = CurvedAnimation(
+    _expandAnimation = CurvedAnimation(
       parent: _controller,
-      curve: Curves.easeInOut,
+      curve: widget.curve,
+    );
+    _rotateAnimation = Tween<double>(begin: 0.0, end: widget.rotationAngle).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: widget.curve,
+      ),
     );
   }
-  
+
   @override
   void dispose() {
     _controller.dispose();
     super.dispose();
   }
-  
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        if (widget.renderOverlay)
+          AnimatedBuilder(
+            animation: _expandAnimation,
+            builder: (context, child) {
+              return Opacity(
+                opacity: _isOpen ? (widget.overlayOpacity ?? 0.8) : 0.0,
+                child: Container(
+                  color: widget.overlayColor ?? Colors.black,
+                ),
+              );
+            },
+          ),
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ...widget.children.map((child) => _buildSpeedDialChild(child)),
+            const SizedBox(height: widget.spacing ?? 16),
+            _buildMainFab(),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMainFab() {
+    return Transform.rotate(
+      angle: widget.useRotationAnimation ? _rotateAnimation.value * 0.0174533 : 0.0,
+      child: FloatingActionButton(
+        onPressed: _toggle,
+        tooltip: widget.tooltip,
+        backgroundColor: _isOpen
+            ? (widget.activeBackgroundColor ?? Colors.red)
+            : (widget.backgroundColor ?? AppConstants.primaryColor),
+        foregroundColor: _isOpen
+            ? (widget.activeForegroundColor ?? Colors.white)
+            : (widget.foregroundColor ?? Colors.white),
+        elevation: widget.elevation,
+        mini: widget.mini,
+        shape: widget.shape,
+        child: _isOpen
+            ? (widget.activeIcon ?? const Icon(Icons.close))
+            : (widget.icon ?? const Icon(Icons.add)),
+      ),
+    );
+  }
+
+  Widget _buildSpeedDialChild(SpeedDialChild child) {
+    return AnimatedBuilder(
+      animation: _expandAnimation,
+      builder: (context, childWidget) {
+        return Transform.translate(
+          offset: Offset(
+            0.0,
+            _expandAnimation.value * -(widget.children.indexOf(child) + 1) * 56.0,
+          ),
+          child: Opacity(
+            opacity: _expandAnimation.value,
+            child: Container(
+              margin: const EdgeInsets.symmetric(vertical: 4),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  if (child.label != null)
+                    Container(
+                      margin: const EdgeInsets.only(right: 8),
+                      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                      decoration: BoxDecoration(
+                        color: Colors.black87,
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: Text(
+                        child.label!,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                  FloatingActionButton(
+                    onPressed: child.onTap,
+                    tooltip: child.tooltip,
+                    backgroundColor: child.backgroundColor ?? AppConstants.primaryColor,
+                    foregroundColor: child.foregroundColor ?? Colors.white,
+                    elevation: child.elevation ?? 4.0,
+                    mini: true,
+                    heroTag: child.heroTag,
+                    child: child.child,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   void _toggle() {
     setState(() {
       _isOpen = !_isOpen;
       if (_isOpen) {
         _controller.forward();
+        widget.onOpen?.call();
       } else {
         _controller.reverse();
+        widget.onClose?.call();
       }
     });
   }
-  
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        ...widget.items.map((item) {
-          return AnimatedBuilder(
-            animation: _animation,
-            builder: (context, child) {
-              return Transform.translate(
-                offset: Offset(0, -_animation.value * 60 * (widget.items.indexOf(item) + 1)),
-                child: Opacity(
-                  opacity: _animation.value,
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 8),
-                    child: FloatingActionButton(
-                      onPressed: () {
-                        item.onPressed();
-                        _toggle();
-                      },
-                      tooltip: item.tooltip,
-                      backgroundColor: item.backgroundColor ?? Colors.white,
-                      foregroundColor: item.foregroundColor ?? Colors.black,
-                      mini: true,
-                      child: Icon(item.icon),
-                    ),
-                  ),
-                ),
-              );
-            },
-          );
-        }).toList(),
-        FloatingActionButton(
-          onPressed: _toggle,
-          tooltip: widget.mainTooltip,
-          backgroundColor: widget.backgroundColor ?? AppConstants.primaryColor,
-          foregroundColor: widget.foregroundColor ?? Colors.white,
-          child: AnimatedRotation(
-            turns: _isOpen ? 0.5 : 0,
-            duration: AppConstants.animationDuration,
-            child: Icon(widget.mainIcon),
-          ),
-        ),
-      ],
-    );
-  }
 }
 
-class SpeedDialItem {
-  final IconData icon;
-  final String tooltip;
-  final VoidCallback onPressed;
+class SpeedDialChild {
+  final Widget child;
+  final VoidCallback onTap;
+  final String? label;
+  final String? tooltip;
   final Color? backgroundColor;
   final Color? foregroundColor;
-  
-  SpeedDialItem({
-    required this.icon,
-    required this.tooltip,
-    required this.onPressed,
+  final double? elevation;
+  final Object? heroTag;
+
+  SpeedDialChild({
+    required this.child,
+    required this.onTap,
+    this.label,
+    this.tooltip,
     this.backgroundColor,
     this.foregroundColor,
+    this.elevation,
+    this.heroTag,
   });
 }
